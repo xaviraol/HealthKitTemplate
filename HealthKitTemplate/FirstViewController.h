ecb3276aa90f75df70e229f923f08470079b3d35
xramoli <xaviramos3@gmail.com>
2016-06-16 14:10:32 +0200

    Initial Commit

---
+++ HealthKitTemplate/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  HealthKitTemplate
+//
+//  Created by Xavier Ramos Oliver on 16/06/16.
+//  Copyright © 2016 SenseHealth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ HealthKitTemplate/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  HealthKitTemplate
+//
+//  Created by Xavier Ramos Oliver on 16/06/16.
+//  Copyright © 2016 SenseHealth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
